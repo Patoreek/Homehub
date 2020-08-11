ddb765b0f65bbdebee6328ad95d80fa3c93a445e
Patrick Minda <patrickminda@hotmail.com>
2020-08-11 20:32:00 +1000

    Added styling to bottom part of whiteboard. all numbers seem to be accurate.

--- src/containers/WhiteboardView/WhiteboardView.js
+++ src/containers/WhiteboardView/WhiteboardView.js
@@ -1,10 +1,10 @@
-import React, { useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 
 import classes from './WhiteboardView.module.scss';
 
 import Whiteboard from '../../components/Whiteboard/Whiteboard';
 
-import {PatTotalContext, NickTotalContext} from '../../context/GlobalContext';
+import {PatTotalContext, NickTotalContext, RentCostContext} from '../../context/GlobalContext';
 
 
 const WhiteboardView = () => {
@@ -13,6 +13,66 @@ const WhiteboardView = () => {
     const [ patTotal, setPatTotal ] = useContext(PatTotalContext);
 
     const [ nickTotal, setNickTotal ] = useContext(NickTotalContext);
+
+    const [ rentCost, setRentCost ] = useContext(RentCostContext);
+
+    const [higherAmount, setHigherAmount] = useState({
+        name:"",
+        total: null
+    });
+
+    const [lowerAmount, setLowerAmount] = useState({
+        name:"",
+        total: null
+    });
+
+    const [formula, setFormula] = useState(null);
+
+    useEffect(() => {
+
+        if (nickTotal > patTotal){
+            setHigherAmount({
+                name: "Nick",
+                total: nickTotal
+            });
+            setLowerAmount({
+                name: "Patrick",
+                total: patTotal
+            })
+
+            setRentCost(205);
+
+            const calcFormula = (nickTotal - patTotal) + rentCost;
+            setFormula(calcFormula.toFixed(2));
+
+
+        } else {
+            setHigherAmount({
+                name: "Patrick",
+                total: patTotal
+            });
+            setLowerAmount({
+                name: "Nick",
+                total: nickTotal
+            })
+
+            const leftOver = patTotal - nickTotal;
+
+            if (leftOver > 0){
+                setRentCost(205 - leftOver);
+                setFormula((205 - leftOver).toFixed(2));
+
+            }
+
+
+        }
+
+
+
+
+    },[nickTotal, patTotal])
+
+
 
 
   return (
@@ -25,10 +85,47 @@ const WhiteboardView = () => {
             <h3 className={classes.nameHeader}>Nick</h3>
             <Whiteboard user="Nick" rent={true}/>
         </div>
+
+
         <div className={classes.finalContainer}>
-            Pat Total = {patTotal}
-            Nick Total = {nickTotal}
+            <div className={classes.higherAmountTotalDiv}>
+                <span className={classes.higherAmountTotalName}>{higherAmount.name}'s Total</span>
+                <span className={classes.higherAmountTotalNum}>${higherAmount.total}</span>
+            </div>
+
+            <div className={classes.lowerAmountTotalDiv}>
+                <span className={classes.lowerAmountTotalName}>{lowerAmount.name}'s Total</span>
+                <span className={classes.lowerAmountTotalNum}>${lowerAmount.total}</span>
+            </div>
+
+            <div className={classes.minusCalcDiv}>
+                <span className={classes.minusCalcName}>{higherAmount.name} Total - {lowerAmount.name} Total</span>
+                <span className={classes.minusCalcNum}>${(higherAmount.total - lowerAmount.total).toFixed(2)} </span>
+            </div>
+
+            {rentCost === 205 && (
+                <div className={classes.rentCalcDiv}>
+                    <span className={classes.rentCalcName}>{higherAmount.name} Total - {lowerAmount.name} Total + Rent (${rentCost})</span>
+                    <span className={classes.rentCalcNum}>${formula}</span>
+                <br/>
+                </div>
+            )}
+            {rentCost === 205 && (
+                <div className={classes.finalCalcDiv}>
+                    <span className={classes.finalCalcName}>Patrick owes Nick </span>
+                    <span className={classes.finalCalcNum}>${formula}</span>
+                </div>
+            )}
+            {rentCost != 205 && (
+                /*I pay Nick this much for rent if I pay more than him that week: ${formula}*/
+                <div className={classes.finalCalcDiv}>
+                <span className={classes.finalCalcName}>Patrick pays Nick this much for rent </span>
+                <span className={classes.finalCalcNum}>${formula}</span>
+            </div>
+            )}
+
         </div>
+        
         
     </div>
   );
--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -6,6 +6,8 @@ export const PatTotalContext = createContext();
 
 export const NickTotalContext = createContext();
 
+export const RentCostContext = createContext();
+
 
 export const GlobalProvider = (props) => {
 
@@ -15,14 +17,19 @@ export const GlobalProvider = (props) => {
 
     const [nickTotal, setNickTotal] = useState(0);
 
+    const [rentCost, setRentCost] = useState(205);
+
+
 
     return (
         <ViewContext.Provider value = {[view, setView]}>
             <PatTotalContext.Provider value = {[patTotal, setPatTotal]}>
                 <NickTotalContext.Provider value = {[nickTotal, setNickTotal]}>
+                <RentCostContext.Provider value = {[rentCost, setRentCost]}>
 
-                {props.children}
+                    {props.children}
 
+                </RentCostContext.Provider>
                 </NickTotalContext.Provider>
             </PatTotalContext.Provider>
         </ViewContext.Provider>
